12dc05bedb3fb42442d6319a68ac15e7a2f19eb7
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-29 19:47:57 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,14 +10,10 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  
-  ss_movies = directors_database[0][:movies]
   row_index = 0 
   
-  
   while row_index < directors_database.length do
-    titles = ss_movies[row_index][:title]
-    puts titles
+    puts directors_database[0][:movies][row_index][:title]
     row_index += 1 
 end
 end 
