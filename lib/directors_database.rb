f18a74395ced4ce2591c2cda8f3c66bc62709a8e
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-29 19:24:42 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -3,5 +3,6 @@ require 'yaml'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-  pretty_print_nds
 end
+
+pretty_print_nds
